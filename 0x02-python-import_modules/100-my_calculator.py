da9880f0430cdf56e7a0b2af5cde32a7dd804b4d
amanabay <amanuelabay22@gmail.com>
2023-03-10 02:19:22 +0000

    Simple calculations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    argc = len(sys.argv) - 1
+    argv = sys.argv
+
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
+        print(argv[2])
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    
+    if argv[2] == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif argv[2] == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    if argv[2] == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    if argv[2] == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
