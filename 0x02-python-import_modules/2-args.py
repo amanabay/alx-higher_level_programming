9c78bd7c28f4504c70da13b77fc5002268183902
amanabay <amanuelabay22@gmail.com>
2023-03-10 01:02:17 +0000

    Remove the default arg from output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,6 @@
 if __name__ == "__main__":
     import sys
 
-    print(f"{len(sys.argv)} arguments:")
-    for i in range(len(sys.argv)):
-        print(f"{i + 1}: {sys.argv[i]}")
+    print(f"{len(sys.argv) - 1} arguments:")
+    for i in range(len(sys.argv) - 1):
+        print(f"{i + 1}: {sys.argv[i + 1]}")
