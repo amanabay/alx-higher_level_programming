bd682cbdc1e2508dbfe2b8a73f6254439d91bf4d
amanabay <amanuelabay22@gmail.com>
2023-03-10 01:11:03 +0000

    Missed a semicolon

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif len(sys.argv) - 1 == 1:
         print("1 argument:")
-    else
+    else:
         print(f"{len(sys.argv) - 1} arguments:")
 
     for i in range(len(sys.argv) - 1):
