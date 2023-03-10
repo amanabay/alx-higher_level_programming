678435a11ed6c036956eab0c929cde490bb3a8bb
amanabay <amanuelabay22@gmail.com>
2023-03-10 01:07:32 +0000

    Fixing edge cases

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,12 @@
 if __name__ == "__main__":
     import sys
 
-    print(f"{len(sys.argv) - 1} arguments:")
+    if len(sys.argv) - 1 == 0:
+        print("0 arguments.")
+    elif len(sys.argv) - 1 == 1:
+        print("1 argument:")
+    else
+        print(f"{len(sys.argv) - 1} arguments:")
+
     for i in range(len(sys.argv) - 1):
         print(f"{i + 1}: {sys.argv[i + 1]}")
