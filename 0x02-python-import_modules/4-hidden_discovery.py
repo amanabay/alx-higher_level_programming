3a6c288dbb922d0cc25d85c0bd050221aa3effc0
amanabay <amanuelabay22@gmail.com>
2023-03-10 01:44:11 +0000

    Defining specific terms

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,8 @@
 
 if __name__ == "__main__":
     import hidden_4
-    
-    for i in range(9, 12):
+
+    for i in range(len(dir(hidden_4))):
+        if dir(hidden_4)[i][0] == '_':
+            continue
         print(dir(hidden_4)[i])
