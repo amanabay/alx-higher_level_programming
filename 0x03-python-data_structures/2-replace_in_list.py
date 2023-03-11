0eb29b42b920083206587777485aafb366c11b03
amanabay <amanuelabay22@gmail.com>
2023-03-11 11:46:33 +0000

    Shouldnt have used the loop

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,6 +6,5 @@ def replace_in_list(my_list, idx, element):
     elif idx > len(my_list):
         return my_list
 
-    for i in range(len(my_list)):
-        if i == idx:
-            my_list[i] = element
+    my_list[idx] = element
+    return my_list
