9c85a3d0e429afdfcb80fcac735ee07413d1091b
amanabay <amanuelabay22@gmail.com>
2023-03-12 13:20:47 +0000

    New in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    cop_list = my_list
+
+    if idx < 0:
+        return cop_list
+    elif idx > len(my_list) - 1:
+        return cop_list
+
+    cop_list[idx] = element
+    return cop_list
