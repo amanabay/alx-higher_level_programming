158b4d7e61f5e60093f41184276244baed300ffd
amanabay <amanuelabay22@gmail.com>
2023-03-12 18:30:48 +0000

    Multiple returns are rewarding

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    mult_ret = len(sentence), sentence[0]
-    return mult_ret
+    if sentence:
+        mult_ret = len(sentence), sentence[0]
+        return mult_ret
+    else:
+        return None
