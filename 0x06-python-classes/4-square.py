0519b9458d0155cea998a3811023165b37407516
amanabay <amanuelabay22@gmail.com>
2023-03-29 19:29:46 +0000

    Getters and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square of a certain size."""
+
+    def __init__(self, size=0):
+        """Initialize new square.
+
+        Args:
+            size (int): Size of square.
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """The size property."""
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self._size = value
+
+    def area(self):
+        """Returns area of square."""
+        return (self.__size * self.__size)
