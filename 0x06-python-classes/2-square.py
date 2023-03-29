d95dd7bda999527b817b31997bd5611f981512c2
amanabay <amanuelabay22@gmail.com>
2023-03-29 19:12:45 +0000

    Add some rainsing of errors

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square of a certain size."""
+
+    def __init__(self, size=0):
+        """Initialize new square.
+
+        Args:
+            size (int): Size of square.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size > 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
