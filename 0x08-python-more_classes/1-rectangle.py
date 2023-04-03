db6c43f514cef5ae0c0455c5ecc379ba8a13b7e4
amanabay <amanuelabay22@gmail.com>
2023-04-03 23:30:25 +0000

    Rectangle with height and width

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle representation"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes new Rectangle.
+        Args:
+            width (int): Width of new rectangle.
+            height (int): Height of new rectangle.
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """The width property."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """The height property."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__height = value
