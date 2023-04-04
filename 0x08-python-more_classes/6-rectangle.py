1d7e545d74071507a2ad0d3c6fd57ab4b3f55be1
amanabay <amanuelabay22@gmail.com>
2023-04-04 00:13:59 +0000

    Use type(self)

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
             width (int): Width of new rectangle
             height (int): Height of new rectangle
         """
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
         self.width = width
         self.height = height
