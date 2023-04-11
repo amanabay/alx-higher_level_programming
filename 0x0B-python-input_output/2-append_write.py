6a49102d4012c88ee82cc58dea6d737ba98783ab
amanabay <amanuelabay22@gmail.com>
2023-04-11 23:58:37 +0000

    Append - fix err

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Function definiton for a appending to file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """ Appends a string to a text file (UTF8) and returns
         the number of characters added
 
