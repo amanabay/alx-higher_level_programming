153c94caaecdd4d737f46423d8fb4a9714f5d111
amanabay <amanuelabay22@gmail.com>
2023-04-11 02:51:09 +0000

    Not implemented...yet

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Base Geometry."""
+
+
+class BaseGeometry:
+    """Represents a base geometry"""
+
+    def area(self):
+        """Not implemented... yet"""
+        raise Exception("area() is not implemented")
