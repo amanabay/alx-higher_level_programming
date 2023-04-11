f889c8f5c0760771216b50b4d710148b2279b814
amanabay <amanuelabay22@gmail.com>
2023-04-11 23:55:36 +0000

    Append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function definiton for a appending to file"""
+
+
+def write_file(filename="", text=""):
+    """ Appends a string to a text file (UTF8) and returns
+        the number of characters added
+
+        Args:
+            filename(str): name of file to be appended to
+            text(str): string to append into file
+        Return:
+            number of characters added
+    """
+    with open(filename, "a", encoding="utf-8") as fp:
+        return fp.write(text)
