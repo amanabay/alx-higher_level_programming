0d5766cd00238cee31793e2aef8d149362f793a9
amanabay <amanuelabay22@gmail.com>
2023-04-13 02:53:50 +0000

    Class student 2

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializer
+        Args:
+            first_name (str): first name of the student
+            last_name (str): last name of the student
+            age (int): age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the json representation of instance
+        Args:
+            attrs (list): a given attributes
+        Returns:
+            (dict) a dictionary
+        """
+        if type(attrs) is list and all([type(x) == str for x in attrs]):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
