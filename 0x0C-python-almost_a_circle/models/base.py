2709ec4c40d12369c861978437292648bba605a2
amanabay <amanuelabay22@gmail.com>
2023-04-17 09:35:51 +0000

    Initial commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Class definition for Base"""
+
+
+class Base:
+    """Base class for all other classes
+
+    Private Class Attributes:
+        __nb_object (int): number of instantiated objects
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializer with id"""
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
