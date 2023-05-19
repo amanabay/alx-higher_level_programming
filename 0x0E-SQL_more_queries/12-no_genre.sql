5afcd98b7121d3342c1820c9f5e2c2c1b4e6e45f
amanabay <amanuelabay22@gmail.com>
2023-05-19 01:23:25 +0000

    List with count

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,8 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-	FROM tv_shows
-	LEFT OUTER JOIN tv_show_genres
-	ON tv_shows.id = tv_show_genres.show_id
-	WHERE tv_show_genres.show_id IS NULL 
-	ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT  tv_genres.name as genre, COUNT(tv_show_genres.genre_id) as number_of_shows
+	FROM tv_show_genres
+	INNER JOIN tv_genres
+	ON tv_genres.id = tv_show_genres.genre_id 
+	GROUP BY tv_genres.name
+	ORDER BY number_of_shows DESC;
