bd5583c55e7c562ed469bc52b5d2f74f9d54a0e1
amanabay <amanuelabay22@gmail.com>
2023-06-13 05:15:26 +0300

    Fix semistandard issue

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,13 +2,9 @@
 // Prints message depending on argument length
 
 if (process.argv.length === 2) {
-    console.log('No argument');
-}
-
-else if (process.argv.length === 3) {
-    console.log('Argument found');
-}
-
-else if (process.argv.length > 3) {
-    console.log('Arguments found');
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
 }
