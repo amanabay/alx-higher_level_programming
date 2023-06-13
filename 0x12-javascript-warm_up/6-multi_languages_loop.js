4df2eb0e25260bd068668472ba3fbda1c25a217a
amanabay <amanuelabay22@gmail.com>
2023-06-13 05:37:16 +0300

    Use array and loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Prints 3 lines: by using an array of string and a loop
+
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+
+while (i < 3) {
+  console.log(strings[i]);
+  i++;
+}
