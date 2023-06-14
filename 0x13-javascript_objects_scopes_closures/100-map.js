bdf61aba37ea4ce7886f8a12bb18f67499ef6045
amanabay <amanuelabay22@gmail.com>
2023-06-14 05:22:05 +0300

    Map to new list

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Imports an array and computes a new array.
+
+const list = require('./100-data').list;
+
+const newList = list.map((value, index) => {
+  return value * index;
+});
+
+console.log(list);
+console.log(newList);
