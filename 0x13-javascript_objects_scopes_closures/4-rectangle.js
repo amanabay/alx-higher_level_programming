fe1c242348b7734037af12fed2bdd83f87c005a2
amanabay <amanuelabay22@gmail.com>
2023-06-14 04:39:22 +0300

    Add rotate and double functions

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+// Define Rectangle class
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let row = 'X';
+    for (let i = 0; i < this.width; i++) {
+      row += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
