5d5bf507b7520c7cd8a42806e238285eb5a51793
amanabay <amanuelabay22@gmail.com>
2023-06-15 05:50:57 +0300

    Fix map issue

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,10 +2,7 @@
 // Imports an array and computes a new array.
 
 const list = require('./100-data.js').list;
-
-const newList = list.map((value, index) => {
-  return value * index;
-});
+const newList = list.map((value, index) => value * index);
 
 console.log(list);
 console.log(newList);
