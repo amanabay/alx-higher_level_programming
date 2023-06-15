7bde6d8929837fb7bb3c5cf203fdcdcf2eedb356
amanabay <amanuelabay22@gmail.com>
2023-06-15 04:43:53 +0300

    Remove condition

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,8 +5,4 @@ function add (a, b) {
   return (a + b);
 }
 
-if (isNaN(Number(process.argv[2])) || isNaN(Number(process.argv[3]))) {
-  console.log('NaN');
-} else {
-  console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
-}
+console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
