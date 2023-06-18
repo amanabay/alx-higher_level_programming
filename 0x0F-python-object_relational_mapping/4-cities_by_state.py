bb55c7313b2c48ca55fd2bec86df10d11e29e56a
amanabay <amanuelabay22@gmail.com>
2023-06-18 03:33:19 +0300

    List all cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+    Script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM cities ORDER BY id ASC"
+
+    cursor.execute(query)
+
+    for city in cursor.fetchall():
+        print(city)
+
+    cursor.close()
+    db.close()
