a1624761970b7aaa519750ec060d754a2c873d3b
amanabay <amanuelabay22@gmail.com>
2023-07-26 01:07:56 +0300

    Return title of movie

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+
+  const json = JSON.parse(body);
+  console.log(json.title);
+});
